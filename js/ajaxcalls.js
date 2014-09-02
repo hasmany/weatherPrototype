073ddfb9e8cff6a1c0c26981c64e59447d052d12
Hubert Nguyen <hubertanguyen@gmail.com>
2014-09-01 23:18:37 -0500

    comparison html and ajax setup for two cities

--- js/ajaxcalls.js
+++ js/ajaxcalls.js
@@ -19,6 +19,26 @@ var ajaxModule = (function() {
         var chart = new Highcharts.Chart(combochartHumidModule.setChartTempOptions(data,unit));
         }
       });
+    },
+
+    ajaxCompare1:function(location,days,unit) {
+      var url = "http://api.openweathermap.org/data/2.5/forecast/daily?q="+location+"&mode=json&units=imperial&cnt="+ days;
+      $.ajax(url,{
+        success: function(data) {
+          window.data1 = data;
+          console.log("comparison1");
+        }
+      });
+    },
+
+    ajaxCompare2:function(location,days,unit) {
+      var url = "http://api.openweathermap.org/data/2.5/forecast/daily?q="+location+"&mode=json&units=imperial&cnt="+ days;
+      $.ajax(url,{
+        success: function(data) {
+          window.data2 = data;
+          console.log("comparison2");
+        }
+      });
     }
   };
 })();
--- js/main.js
+++ js/main.js
@@ -23,8 +23,19 @@ $(document).ready(function() {
 
    $("#compare").on("click",function() {
      var location = $("#location").val();
+     var location2 = $("#location2").val();
      var days = $("#amount").val();
+     var check = location && location2;
+     if (!check) {
+       alert("Please type in two cities to compare")
+     }
+     else {
+      ajaxModule.ajaxCompare1(location,days,unit);
+      ajaxModule.ajaxCompare2(location2,days,unit);
+      alert("two cities nice!");
+     }
      var unit = "";
+
      // need to make 2 ajax calls
      // each one will make adjusments to the
      // chart config
